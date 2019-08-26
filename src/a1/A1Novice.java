000f7cce46614244d64beb8b6e1715e26b36d62e
xingdali <xingdali@live.unc.edu>
2019-08-25 20:12:57 -0400

    finished the Novice.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,20 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int numberOfCostumers = scan.nextInt();
+		for (int i = 0; i < numberOfCostumers; i++) { //go through every costumer.
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numberOfGoods = scan.nextInt();
+			double sum = 0;
+			for (int j = 0; j < numberOfGoods; j++) { //go through every single good
+				int number = scan.nextInt();
+				String nameOfGoods = scan.next();
+				double price = scan.nextDouble();
+				sum += number * price; // sum the total price
+			}
+			System.out.println(firstName.substring(0, 1) + ". " + lastName + ": " + sum); //output bills
+		}
+		scan.close();
 	}
 }
